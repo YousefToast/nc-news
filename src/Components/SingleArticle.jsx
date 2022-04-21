f3ce40555702258b6a8bcdf63dce33bf12d57428
YousefToast <yousef.abobaker98@hotmail.com>
2022-04-21 13:09:09 +0100

    added vote component

--- src/Components/SingleArticle.jsx
+++ src/Components/SingleArticle.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchArticlesById } from "../utils/api";
 import Comments from "./Comments";
+import Votes from "./Votes";
 import "../styling/SingleArticle.css";
 
 const SingleArticle = () => {
@@ -21,8 +22,14 @@ const SingleArticle = () => {
         <p>{article.topic}</p>
         <h3>{article.title}</h3>
         <p>{article.body}</p>
-
-        <p>Votes: {article.votes}</p>
+        <div className="votesWrapper">
+          <Votes
+            votes={article.votes}
+            article_id={article_id}
+            setArticle={setArticle}
+            article={article}
+          />
+        </div>
         <p>Comments: {article.comment_count}</p>
         <p>posted: {article.created_at}</p>
         <hr></hr>
---
+++ src/Components/Votes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styling/SingleArticle.css";
+import { patchLikesById } from "../utils/api";
+
+const Votes = ({ votes, article_id, setArticle, article }) => {
+  const handleLike = async (inc) => {
+    await patchLikesById(article_id, inc);
+    setArticle({ ...article, votes: votes + inc });
+  };
+
+  return (
+    <div className="voteButton">
+      <button onClick={() => handleLike(1)}>Like</button>
+      <p>{votes}</p>
+      <button onClick={() => handleLike(-1)}>Dislike</button>
+    </div>
+  );
+};
+
+export default Votes;
