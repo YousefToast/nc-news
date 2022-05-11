20fba955d16c25d398021f3cd8647b7f36cca813
YousefToast <yousef.abobaker98@hotmail.com>
2022-05-11 20:17:44 +0300

    added delete comments

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -1,38 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchCommentsById } from "../utils/api";
 import "../styling/SingleArticle.css";
 import DeleteComment from "./DeleteComment";
 
 const Comments = ({ article_id, comments, setComments }) => {
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetchCommentsById(article_id).then((res) => {
       setComments(res.comments);
+      setIsLoading(false);
     });
   }, [article_id, setComments]);
 
-  return (
-    <section>
-      <ul className="commentList">
-        {comments.map((comment) => {
-          return (
-            <li key={comment.comment_id}>
-              <p>posted by: {comment.author}</p>
-              <p>{comment.body}</p>
-              <p>Votes: {comment.votes}</p>
-              <div className="underline">
-                <DeleteComment
-                  comment_id={comment.comment_id}
-                  setComments={setComments}
-                  author={comment.author}
-                  comments={comments}
-                />
-              </div>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
+  if (isLoading) return <p>Loading Comments...</p>;
+  else {
+    return (
+      <section>
+        <ul className="commentList">
+          {comments.map((comment) => {
+            return (
+              <li key={comment.comment_id}>
+                <p>posted by: {comment.author}</p>
+                <p>{comment.body}</p>
+                <p>Votes: {comment.votes}</p>
+                <div className="underline">
+                  <DeleteComment
+                    comment_id={comment.comment_id}
+                    setComments={setComments}
+                    author={comment.author}
+                    comments={comments}
+                  />
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
+  }
 };
 
 export default Comments;
